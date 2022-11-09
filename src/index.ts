bd197eb6d0f062502d3edbaa41f9d5bb805912be
Shish <shish@shishnet.org>
2022-11-09 14:49:58 +0000

    types

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
+import type { Dispatch, Effect, Subscription, Unsubscribe } from "hyperapp";
+
+type SurviveHMRProps<S> = {
+  module: any;
+  fresh_inits: Array<Effect<S>>;
+};
+
 const DEBUG = false;
 
-function survivor(dispatch, props) {
+function survivor<S>(
+  dispatch: Dispatch<S>,
+  props: SurviveHMRProps<S>
+): Unsubscribe {
   if (DEBUG) console.log("Checking hmr", props.module);
   let hmred = false;
 
   if (typeof props.module !== "undefined" && props.module.hot) {
-    props.module.hot.dispose(function(data) {
-      dispatch(function(state) {
+    props.module.hot.dispose(function (data) {
+      dispatch(function (state) {
         if (DEBUG) console.log("Saving state");
         data.saved_state = state;
         return state;
       });
     });
-    props.module.hot.accept(function(getParents) {
-      dispatch(function(state) {
+    props.module.hot.accept(function (getParents) {
+      dispatch(function (state) {
         if (!props.module.hot.data || !props.module.hot.data.saved_state) {
           if (DEBUG) console.log("No saved state");
           return state;
@@ -25,18 +35,23 @@ function survivor(dispatch, props) {
     });
   }
 
-  requestAnimationFrame(function() {
+  requestAnimationFrame(function () {
     if (DEBUG) console.log("HMR activity detected", hmred);
     if (!hmred) {
       if (DEBUG)
         console.log("HMR didn't do anything, running original init functions");
-      dispatch(function(state) {
+      dispatch(function (state) {
         return [state, ...props.fresh_inits];
       });
     }
   });
+
+  return () => {};
 }
 
-export function SurviveHMR(module, fresh_inits) {
+export function SurviveHMR<S>(
+  module: any,
+  fresh_inits: Array<Effect<S>>
+): Subscription<S, SurviveHMRProps<S>> {
   return [survivor, { module, fresh_inits }];
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -7,9 +7,9 @@ function dispatch(action, args) {
 describe("survive_hmr", () => {
   it("Should save state before HMR, and reload it after", () => {
     // TODO: test
-    let [effect, props] = SurviveHMR(module, []);
-    expect(effect).not.toEqual(null);
+    let [sub, props] = SurviveHMR(module, []);
+    expect(sub).not.toEqual(null);
     expect(props).not.toEqual(null);
-    effect(dispatch, props);
+    sub(dispatch, props);
   });
 });
